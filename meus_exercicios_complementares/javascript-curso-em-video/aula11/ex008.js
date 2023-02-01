e3dae2b87fb72321a253f669969a8d754616879d
Aluno <kaua.moreno2005@gmail.com>
2023-02-01 09:36:31 -0300

    upload dos meus exercicios complementares

---
+++ meus_exercicios_complementares/javascript-curso-em-video/aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente')
+console.log('')
+
+var velocidade = 62;
+
+if(velocidade > 60){
+    console.log('Você está acima da velocidade permitida');
+}else{
+    console.log('Dirija sempre usando cinto de segurança')
+}
---
+++ meus_exercicios_complementares/javascript-curso-em-video/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "Brasil";
+
+if(pais == "Brasil"){
+    console.log("Você é brasileiro")
+}else{
+    console.log("Você é extrangeiro")
+}
